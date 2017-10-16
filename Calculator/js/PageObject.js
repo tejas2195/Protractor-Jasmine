cfb8708ff6ddacc0939f2da042d6ddf4ded722d8
Tejas <tejas@nineleaps.com>
2017-10-16 19:27:44 +0530

    First commit

---
+++ Calculator/js/PageObject.js
@@ -0,0 +1,15 @@
+﻿'use strict';
+var Calculator = function () {
+    browser.get('http://juliemr.github.io/protractor-demo/');
+};
+Calculator.prototype = Object.create({}, {
+    first: { get: function () { return element(by.model('first')); } },
+    second: { get: function () { return element(by.model('second')); } },
+    firstValue: { value: function (keys) { return this.first.sendKeys(keys); } },
+    secondValue: { value: function (keys) { return this.second.sendKeys(keys); } },
+    //operator: { get: function () { return element(by.model('operator')) } },
+    dropDown: { value: function (values) { return element(by.cssContainingText('option', values)).click(); } },
+   button: { get: function () { return element(by.buttonText('Go!')); } },
+    output: { get: function () { return (element(by.binding('latest')).getText()); } }
+});
+module.exports = Calculator;
---
+++ Calculator/js/Spec.js
@@ -0,0 +1,19 @@
+﻿'use strict';
+
+var Calculator = require('./PageObject.js');
+
+describe('To add', function () {
+    var calc;
+    beforeEach(function () {
+        calc = new Calculator();
+    });
+    it('add value', function () {
+        calc.firstValue('10');
+        calc.secondValue('2');
+        calc.dropDown('/');
+       // element(by.cssContainingText('option', '-')).click();
+       // element(by.buttonText('Go!')).click(); 
+        calc.button.click();
+        expect(calc.output).toEqual('5');
+    });
+});
